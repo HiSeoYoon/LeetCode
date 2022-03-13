34b87746d612e599ec1273ccd19a6439211d96a9
sy <hi.seoyoon22@gmail.com>
2022-03-13 14:10:23 +0900

    S0387 Use String type and optimize the code.

--- src/S0387_First_Unique_Character_in_a_String/Solution.java
+++ src/S0387_First_Unique_Character_in_a_String/Solution.java
@@ -1,8 +1,6 @@
 package S0387_First_Unique_Character_in_a_String;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
 
@@ -14,15 +12,13 @@ public class Solution {
 
     public static int firstUniqChar(String s){
         LinkedHashMap<Character, Integer> cntChar = new LinkedHashMap<>();
-
-        char[] strArr = s.toCharArray();
-
-        for(int i = 0; i<strArr.length; i++){
-            if(cntChar.containsKey(strArr[i])){
-                cntChar.put(strArr[i], -1);
+        for(int i = 0; i<s.length(); i++){
+            char curChar = s.charAt(i);
+            if(cntChar.containsKey(curChar)){
+                cntChar.put(curChar, -1);
             }
             else{
-                cntChar.put(strArr[i], i);
+                cntChar.put(curChar, i);
             }
         }
         for(Map.Entry<Character, Integer> entry : cntChar.entrySet()){
