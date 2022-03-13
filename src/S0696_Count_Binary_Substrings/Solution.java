f395a9d69509defb53323f23a62b4f3dc1835da1
sy <hi.seoyoon22@gmail.com>
2022-03-13 14:12:15 +0900

    S0696 Use String type and optimize the code.

--- src/S0696_Count_Binary_Substrings/Solution.java
+++ src/S0696_Count_Binary_Substrings/Solution.java
@@ -1,6 +1,7 @@
 package S0696_Count_Binary_Substrings;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Solution {
 
@@ -11,21 +12,22 @@ public class Solution {
     }
 
     public static int countBinarySubstrings(String s){
-        char[] strArr = s.toCharArray();
         int retVal = 0;
         ArrayList<Integer> cntList = new ArrayList<>();
 
         int trueCnt = 0;
         int falseCnt = 0;
-        for(int i = 0; i<strArr.length; i++){
-            if(strArr[i] == '1'){
+        for(int i = 0; i<s.length(); i++){
+            //int curVal = s.charAt(i) == '1'? 1 : 0;
+            char curChar = s.charAt(i);
+            if(0 == Character.compare(curChar, '1')){
                 trueCnt++;
             }
             else{
                 falseCnt++;
             }
-            if((i == strArr.length - 1 || 0!=Character.compare(strArr[i], strArr[i + 1]))){
-                if(strArr[i] == '1'){
+            if(i == s.length() - 1 || 0!=Character.compare(curChar, s.charAt(i + 1))){
+                if(Character.compare(curChar,'1') == 0){
                     cntList.add(trueCnt);
                     System.out.println("Add True List Val :" + trueCnt);
                     trueCnt = 0;
@@ -39,7 +41,7 @@ public class Solution {
         }
 
         for(int j = 0; j<cntList.size() - 1; j++){
-            if(cntList.get(j)==cntList.get(j+1)){
+            if(Objects.equals(cntList.get(j), cntList.get(j + 1))){
                 retVal += cntList.get(j);
             }
             else if(cntList.get(j)>cntList.get(j+1)){
