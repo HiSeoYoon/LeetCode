f1679d4f3d314ca37deb3242aa528acd12897363
sy <hi.seoyoon22@gmail.com>
2022-03-27 13:30:48 +0900

    0070_Update relative positions of the remaining characters

--- src/S0392_Is_Subsequence/Solution.java
+++ src/S0392_Is_Subsequence/Solution.java
@@ -2,17 +2,20 @@ package S0392_Is_Subsequence;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "abc";
-        String t = "ahbgdc";
+        String s = "aaaabc";
+        String t = "aabbcc";
         boolean output = isSubsequence(s, t);
         System.out.println(output);
     }
 
     public static boolean isSubsequence(String s, String t) {
+        int prevIndex = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (0 > t.indexOf(s.charAt(i))) {
+            int findIndex = t.indexOf(s.charAt((i)), prevIndex);
+            if (0 > findIndex) {
                 return false;
             }
+            prevIndex = findIndex + 1;
         }
         return true;
     }
