9b4075df33cfc30f2b3969ff6e7df47baad2f1d9
sy <hi.seoyoon22@gmail.com>
2022-04-03 14:00:36 +0900

    0075 Update Leetcode Solution

--- src/S0075_Sort_Colors/Solution.java
+++ src/S0075_Sort_Colors/Solution.java
@@ -23,4 +23,22 @@ public class Solution {
             System.out.println(nums[k]);
         }
     }
+
+    public static void LeetCode_sortColors(int[] nums) {
+        int p0 = 0, curr = 0;
+        int p2 = nums.length - 1;
+
+        int tmp;
+        while (curr <= p2) {
+            if (nums[curr] == 0) {
+                tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            } else if (nums[curr] == 2) {
+                tmp = nums[curr];
+                nums[curr] = nums[p2];
+                nums[p2--] = tmp;
+            } else curr++;
+        }
+    }
 }
