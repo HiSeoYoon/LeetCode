d304fd1d1034662cedcd38460706b67020efdf1c
sy <hi.seoyoon22@gmail.com>
2022-04-03 12:50:49 +0900

    0053 Update Leetcode Solution

--- src/S0053_Maximum_Subarray_Solution/Solution.java
+++ src/S0053_Maximum_Subarray_Solution/Solution.java
@@ -18,4 +18,54 @@ public class Solution {
         }
         return maxSubarray;
     }
+
+    public static int kadansAlgorithm(int[] nums) {
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+        return maxSubarray;
+    }
+
+    private static int[] numsArray;
+
+    public static int divideConquer(int[] nums) {
+        numsArray = nums;
+        return findBestSubarray(0, numsArray.length - 1);
+    }
+
+    private static int findBestSubarray(int left, int right) {
+        if (left > right) {
+            return Integer.MIN_VALUE;
+        }
+
+        int mid = Math.floorDiv(left + right, 2);
+        int curr = 0;
+        int bestLeftSum = 0;
+        int bestRightSum = 0;
+
+        for (int i = mid - 1; i >= left; i--) {
+            curr += numsArray[i];
+            bestLeftSum = Math.max(bestLeftSum, curr);
+        }
+
+        curr = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            curr += numsArray[i];
+            bestRightSum = Math.max(bestRightSum, curr);
+        }
+        int bestCombinedSum = numsArray[mid] + bestLeftSum + bestRightSum;
+
+        int leftHalf = findBestSubarray(left, mid - 1);
+        int rightHalf = findBestSubarray(mid + 1, right);
+
+        return Math.max(bestCombinedSum, Math.max(leftHalf, rightHalf));
+    }
+
+
 }
