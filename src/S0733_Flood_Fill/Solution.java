f5b869485b2d03c4f9ff2d89187dd83199d55389
sy <hi.seoyoon22@gmail.com>
2022-04-11 20:56:28 +0900

    0733 Flood_Fill Solution

---
+++ src/S0733_Flood_Fill/Solution.java
@@ -0,0 +1,25 @@
+package S0733_Flood_Fill;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[][] input = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        floodFill(input, 1, 1, 2);
+    }
+
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        return image;
+    }
+
+    public static void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) dfs(image, r - 1, c, color, newColor);
+            if (c >= 1) dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.length) dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].length) dfs(image, r, c + 1, color, newColor);
+        }
+    }
+}
