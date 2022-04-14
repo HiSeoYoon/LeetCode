b2774cfcdba213ee49794b64d7b55028236e9ed1
sy <hi.seoyoon22@gmail.com>
2022-04-14 22:24:28 +0900

    0020 Update LeetCode Solution

--- src/S0020_Valid_Parentheses/Solution.java
+++ src/S0020_Valid_Parentheses/Solution.java
@@ -1,41 +1,40 @@
 package S0020_Valid_Parentheses;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Solution {
     public static void main(String[] args) {
         String input = "()(}";
-        System.out.println(isValid(input));
+        //System.out.println(isValid(input));
     }
 
-    public static boolean isValid(String s) {
-        char smallPar = ')';
-        char middlePar = '}';
-        char largePar = ']';
+    private HashMap<Character, Character> mappings;
 
-        int findIndex = 0;
+    public Solution() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
 
         for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-            switch (s.charAt(i)) {
-                case '(':
-                    findIndex = s.indexOf(smallPar, findIndex + 1);
-                    if (findIndex < 0) {
-                        return false;
-                    }
-                case '{':
-                    findIndex = s.indexOf(middlePar, findIndex + 1);
-                    if (findIndex < 0) {
-                        return false;
-                    }
-                case '[':
-                    findIndex = s.indexOf(largePar, findIndex + 1);
-                    if (findIndex < 0) {
-                        return false;
-                    }
-                default:
-                    continue;
+            if (this.mappings.containsKey(c)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
             }
         }
 
-        return true;
+        return stack.isEmpty();
     }
 }
