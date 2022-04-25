71bffcf3e7778f02eb31fc76b0de454cad17072a
sy <hi.seoyoon22@gmail.com>
2022-04-25 23:32:28 +0900

    0155 Min_Stack Solution

---
+++ src/S0155_Min_Stack/Solution.java
@@ -0,0 +1,39 @@
+package S0155_Min_Stack;
+
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MinStack {
+    private Stack<int[]> stack = new Stack<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+            return;
+        }
+
+        int currentMin = stack.peek()[1];
+        stack.push(new int[]{val, Math.min(val, currentMin)});
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
