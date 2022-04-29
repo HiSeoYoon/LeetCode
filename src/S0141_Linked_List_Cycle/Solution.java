a6eabc25d640a3361d92f2804eedc96c659f2ae6
sy <hi.seoyoon22@gmail.com>
2022-04-29 22:31:41 +0900

    0141 Linked_List_Cycle Solution

--- src/S0141_Linked_List_Cycle/Solution.java
+++ src/S0141_Linked_List_Cycle/Solution.java
@@ -23,4 +23,21 @@ public class Solution {
         }
         return false;
     }
+
+    public static boolean hasCycle_2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
 }
