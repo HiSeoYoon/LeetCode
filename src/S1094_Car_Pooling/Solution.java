c31ab509f22de71d0a74c21a1087d610382115f4
sy <hi.seoyoon22@gmail.com>
2022-05-13 23:55:48 +0900

    1094 Update Car_Pooling Leetcode Solution

--- src/S1094_Car_Pooling/Solution.java
+++ src/S1094_Car_Pooling/Solution.java
@@ -6,10 +6,10 @@ import java.util.TreeMap;
 public class Solution {
     public static void main(String[] args) {
         int[][] input = {{2, 5, 8}, {3, 3, 7}, {1, 3, 5}};
-        carPooling(input, 4);
+        carPooling_2(input, 4);
     }
 
-    public static boolean carPooling(int[][] trips, int capacity) {
+    public static boolean carPooling_1(int[][] trips, int capacity) {
         Map<Integer, Integer> timestamp = new TreeMap<>();
         for (int[] trip : trips) {
             int startPassenger = timestamp.getOrDefault(trip[1], 0) + trip[0];
@@ -21,6 +21,22 @@ public class Solution {
         int usedCapacity = 0;
         for (int passengerChange : timestamp.values()) {
             usedCapacity += passengerChange;
+            if (usedCapacity > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean carPooling_2(int[][] trips, int capacity) {
+        int[] timestamp = new int[1001];
+        for (int[] trip : trips) {
+            timestamp[trip[1]] += trip[0];
+            timestamp[trip[2]] += trip[0];
+        }
+        int usedCapacity = 0;
+        for (int number : timestamp) {
+            usedCapacity += number;
             if (usedCapacity > capacity) {
                 return false;
             }
