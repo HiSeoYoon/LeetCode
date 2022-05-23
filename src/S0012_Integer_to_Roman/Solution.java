eccae79c516ef5e2058eba2a24c79109b34ab251
sy <hi.seoyoon22@gmail.com>
2022-05-23 22:29:08 +0900

    0012 Integer_to_Roman Solution

---
+++ src/S0012_Integer_to_Roman/Solution.java
@@ -0,0 +1,24 @@
+package S0012_Integer_to_Roman;
+
+public class Solution {
+    public static void main(String[] args) {
+        String output = intToRoman(3);
+        System.out.println(output);
+    }
+
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        // Loop through each symbol, stopping if num becomes 0.
+        for (int i = 0; i < values.length && num > 0; i++) {
+            // Repeat while the current symbol still fits into num.
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
