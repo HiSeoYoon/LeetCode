21e06fe83d5479f71b8c6c4385d9cf873f4e87a6
sy <hi.seoyoon22@gmail.com>
2022-06-03 08:52:50 +0900

    0061 Rotate_List Solution

---
+++ src/S0061_Rotate_List/Solution.java
@@ -0,0 +1,37 @@
+package S0061_Rotate_List;
+
+import Common.LinkedList.LinkedListCreator;
+import Common.LinkedList.ListNode;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode input = LinkedListCreator.createLinkedLists("1,2,3,4,5");
+        rotateRight(input, 2);
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        // base cases
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        // close the linked list into the ring
+        ListNode old_tail = head;
+        int n;
+        for (n = 1; old_tail.next != null; n++)
+            old_tail = old_tail.next;
+        old_tail.next = head;
+
+        // find new tail : (n - k % n - 1)th node
+        // and new head : (n - k % n)th node
+        ListNode new_tail = head;
+        for (int i = 0; i < n - k % n - 1; i++)
+            new_tail = new_tail.next;
+        ListNode new_head = new_tail.next;
+
+        // break the ring
+        new_tail.next = null;
+
+        return new_head;
+    }
+
+}
