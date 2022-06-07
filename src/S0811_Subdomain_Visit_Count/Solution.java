13aa71149e59115419e525a0b5e8bbbbd6ddb136
sy <hi.seoyoon22@gmail.com>
2022-06-07 23:22:55 +0900

    0811 Subdomain_Visit_Count Solution

---
+++ src/S0811_Subdomain_Visit_Count/Solution.java
@@ -0,0 +1,32 @@
+package S0811_Subdomain_Visit_Count;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] input = {"9001 discuss.leetcode.com"};
+        subdomainVisits(input);
+    }
+
+    public static List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> counts = new HashMap();
+        for (String domain : cpdomains) {
+            String[] cpinfo = domain.split("\\s+");
+            String[] frags = cpinfo[1].split("\\.");
+            int count = Integer.valueOf(cpinfo[0]);
+            String cur = "";
+            for (int i = frags.length - 1; i >= 0; --i) {
+                cur = frags[i] + (i < frags.length - 1 ? "." : "") + cur;
+                counts.put(cur, counts.getOrDefault(cur, 0) + count);
+            }
+        }
+
+        List<String> ans = new ArrayList();
+        for (String dom : counts.keySet())
+            ans.add("" + counts.get(dom) + " " + dom);
+        return ans;
+    }
+}
