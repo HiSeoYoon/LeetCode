c30b72d1b5fff1446dcc8db7843b631193656d10
sy <hi.seoyoon22@gmail.com>
2022-06-29 23:53:09 +0900

    79 Update Word_Search Solution

--- src/S0079_Word_Search/Solution.java
+++ src/S0079_Word_Search/Solution.java
@@ -16,12 +16,12 @@ public class Solution {
 
         for (int row = 0; row < this.ROWS; ++row)
             for (int col = 0; col < this.COLS; ++col)
-                if (this.backtrack(row, col, word, 0))
+                if (this.backtrack_1(row, col, word, 0))
                     return true;
         return false;
     }
 
-    protected boolean backtrack(int row, int col, String word, int index) {
+    protected boolean backtrack_1(int row, int col, String word, int index) {
         /* Step 1). check the bottom case. */
         if (index >= word.length())
             return true;
@@ -39,7 +39,7 @@ public class Solution {
         int[] rowOffsets = {0, 1, 0, -1};
         int[] colOffsets = {1, 0, -1, 0};
         for (int d = 0; d < 4; ++d) {
-            ret = this.backtrack(row + rowOffsets[d], col + colOffsets[d], word, index + 1);
+            ret = this.backtrack_1(row + rowOffsets[d], col + colOffsets[d], word, index + 1);
             if (ret)
                 break;
         }
@@ -47,5 +47,32 @@ public class Solution {
         /* Step 4). clean up and return the result. */
         this.board[row][col] = word.charAt(index);
         return ret;
+    }
+
+    protected boolean backtrack_2(int row, int col, String word, int index) {
+        /* Step 1). check the bottom case. */
+        if (index >= word.length())
+            return true;
+
+        /* Step 2). Check the boundaries. */
+        if (row < 0 || row == this.ROWS || col < 0 || col == this.COLS
+                || this.board[row][col] != word.charAt(index))
+            return false;
+
+        /* Step 3). explore the neighbors in DFS */
+        // mark the path before the next exploration
+        this.board[row][col] = '#';
+
+        int[] rowOffsets = {0, 1, 0, -1};
+        int[] colOffsets = {1, 0, -1, 0};
+        for (int d = 0; d < 4; ++d) {
+            if (this.backtrack_2(row + rowOffsets[d], col + colOffsets[d], word, index + 1))
+                // return without cleanup
+                return true;
+        }
+
+        /* Step 4). clean up and return the result. */
+        this.board[row][col] = word.charAt(index);
+        return false;
     }
 }
